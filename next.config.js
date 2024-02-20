e5028935f7e7e76b7c1c527fc6387f7af0320f90
Patrick Murimi <kabugap02@gmail.com>
2024-02-20 22:48:52 +0300

    feat: Edit config

--- next.config.js
+++ next.config.js
@@ -3,20 +3,13 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/pages/api/graphql.ts",
+        source: "/pages/api/:path*", // Matches all API routes
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
           {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            key: "Access-Control-Allow-Origin",
+            value: process.env.ALLOWED_CORS_ORIGINS,
           },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
+          // Add other CORS headers if needed (e.g., Access-Control-Allow-Methods, Access-Control-Allow-Headers)
         ],
       },
     ];
