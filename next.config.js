29da773761b08016538dd85def44f5232188f003
Patrick Murimi <kabugap02@gmail.com>
2024-02-23 15:43:37 +0300

    fix: CORS

--- next.config.js
+++ next.config.js
@@ -1,29 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/graphql",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://contribuverse.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -5,12 +5,19 @@ import prisma from "@/prisma/db";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Context } from "@/types";
+import allowCors from "../../utils/cors";
 
-const apolloServer = new ApolloServer<Context>({ typeDefs, resolvers });
-export default startServerAndCreateNextHandler(apolloServer, {
+const apolloServer = new ApolloServer<Context>({
+  typeDefs,
+  resolvers,
+  introspection: true,
+});
+const handler = startServerAndCreateNextHandler(apolloServer, {
   context: async (req: NextApiRequest, res: NextApiResponse) => ({
     req,
     res,
     prisma,
   }),
 });
+
+export default allowCors(handler);
